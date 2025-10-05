6f17decb6c5749d3b95320d3f1bc57c918470e45
ucef-h <ucefh@live.com>
2025-10-05 17:48:02 +0200

    dynamic version

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
-__version__ = "1.0.5"
+from importlib import metadata
+
+try:
+    # The distribution name is the same as specified in pyproject.toml
+    __version__: str = metadata.version("fastapi-app")
+except metadata.PackageNotFoundError:
+    # When running from source without installation, provide a fallback
+    __version__ = "0.0.0"
+
+__all__ = ["__version__"]
